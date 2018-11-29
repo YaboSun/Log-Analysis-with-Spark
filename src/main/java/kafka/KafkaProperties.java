0c9e2c1776069e3e0244071fddb3e0189fc63b65
Yabosun <S&Damon320193>
2018-11-29 16:30:00 +0800

    build project structure

---
+++ src/main/java/kafka/KafkaProperties.java
@@ -0,0 +1,1 @@
+package kafka;/** * @author YaboSun */public class KafkaProperties {    public static final String ZK = "192.168.1.102:2181";    public static final String TOPIC = "hello_topic";    public static final String BROKER_LIST = "192.168.1.102:9092";    public static final String GROUP_ID = "test_group1";}
