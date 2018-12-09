d67753bb1b7553f0b96896d911493fc20dcd1cbe
Yabosun <yabosun@163.com>
2018-12-09 20:35:27 +0800

    测试完成使用kafka sink收集flume输出的信息

--- src/test/java/LoggerGenerator.java
+++ src/test/java/LoggerGenerator.java
@@ -12,7 +12,7 @@ public class LoggerGenerator {
         int index = 0;
         while (true) {
             Thread.sleep(1000);
-            logger.info("current value is : " + index++);
+            logger.info("value: " + index++);
         }
     }
 }
